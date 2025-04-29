73ebdc4d4091c98570ba0b9621f84acc759f370d
Martin <martin_lenart@icloud.com>
2025-04-29 10:34:17 +0200

    update

--- Playground_2025_2_teacher/App1/assets/js/table.js
+++ Playground_2025_2_teacher/App1/assets/js/table.js
@@ -9,7 +9,22 @@ for (let index = 0; index < 10; index++) {
     pets.push({name: seeder.petName, age: randomNumber(0,20) });
 }
 
-pets.forEach(pet => console.log(`${pet.name} is ${pet.age} years old.`));
+
+
+for (const pet of pets) {
+    console.log(`${pet.name} is ${pet.age} years old.`);
+}
+
 
 let table = document.querySelector(".TableContainer");
+
+let div = document.createElement("div");
+div.innerText = "pizza";
+div.classList.add("TableItem");
+table.appendChild(div);
+
+
+
+
+
 let i = 0;
