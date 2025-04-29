d00b847fe6beb4f5b338720678973d95062426ae
Martin <martin_lenart@icloud.com>
2025-04-29 09:01:15 +0200

    update

---
+++ Playground_2025_2_teacher/App1/assets/js/table.js
@@ -0,0 +1,12 @@
+'use strict';
+import {seedGenerator, uniqueId, randomNumber, deepCopy} from '../../../../SeidoHelpers/seido-helpers.js';
+
+let seeder = new seedGenerator();
+
+let pets = [];
+for (let index = 0; index < 50; index++) {
+    
+    pets.push({name: seeder.petName, age: randomNumber(0,20) });
+}
+
+pets.forEach(pet => console.log(`${pet.name} is ${pet.age} years old.`));
