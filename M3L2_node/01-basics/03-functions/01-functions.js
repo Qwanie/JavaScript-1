ac891993616a7e32f3d79ce2dce220024aee2fe8
Martin <martin_lenart@icloud.com>
2025-05-09 15:12:28 +0200

    update

--- M3L2_node/01-basics/03-functions/01-functions.js
+++ M3L2_node/01-basics/03-functions/01-functions.js
@@ -1,14 +1,14 @@
 'use strict';
 import {seedGenerator, uniqueId, randomNumber, deepCopy, isEqual} from '../../../SeidoHelpers/seido-helpers.js';
 
-console.group('Standard function')
+console.log('Standard function')
 function createFriend(_seeder)
 {
     const f = {name: _seeder.fullName};
     return f;
 }
 
-console.group('Function expression');
+console.log('Function expression');
 const createAnotherFriend = function (_seeder)
 {
     const f = {};
@@ -16,9 +16,10 @@ const createAnotherFriend = function (_seeder)
     return f;
 }
 
+console.log('Function delegate');
 const createYetAnotherFriend = createAnotherFriend;
 
-console.group('Arrow function')
+console.log('Arrow function')
 const createYetYetAnotherFriend = (_seeder) => {
     const f = {};
     f.name = _seeder.fullName;
